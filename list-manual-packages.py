d8e0e16f4f68648ee55b17a2a770df7272b5919f
David Osolkowski <qidydl@gmail.com>
2020-05-02 20:55:15 -0400

    Better filtering and comments

--- list-manual-packages.py
+++ list-manual-packages.py
@@ -4,38 +4,50 @@ from apt import cache
 
 aptCache = cache.Cache()
 
-# All manually-installed packages
-manual = {
+# All installed packages
+installed = {
     pkg
     for pkg in aptCache
-    if pkg.is_installed and not pkg.is_auto_installed
+    if pkg.is_installed
 }
 
-# *All* dependencies (manual or otherwise!) of manually-installed packages
+installedNames = {pkg.name for pkg in installed}
+
+# All installed dependencies of installed packages
 depends = {
     dep_pkg.name
-    for pkg in manual
-    for dep in pkg.installed.get_dependencies('PreDepends', 'Depends')#, 'Recommends')
+    for pkg in installed
+    for dep in pkg.installed.get_dependencies('PreDepends', 'Depends', 'Recommends')
     for dep_pkg in dep
+    if dep_pkg.name in installedNames
+}
+
+# All installed suggestions of installed packages
+suggests = {
+    dep_pkg.name
+    for pkg in installed
+    for dep in pkg.installed.get_dependencies('Suggests')
+    for dep_pkg in dep
+    if dep_pkg.name in installedNames
 }
 
 # All manually-installed packages that nothing installed depends on
 manualRoots = [
-    pkg.name
-    for pkg in manual
-    if pkg.name not in depends
+    pkg.name + (" (SUGGESTED)" if pkg.name in suggests else "")
+    for pkg in installed
+    if not pkg.is_auto_installed and pkg.name not in depends
 ]
 
 manualRoots.sort()
 
 print('\n'.join(manualRoots))
 
-manualNotRoots = [
+manualDepends = [
     pkg.name
-    for pkg in manual
-    if pkg.name in depends
+    for pkg in installed
+    if not pkg.is_auto_installed and pkg.name in depends
 ]
 
-manualNotRoots.sort()
+manualDepends.sort()
 
-print("Manual depended on by something: " + ', '.join(manualNotRoots))
+print("Manual depended on by something: " + ', '.join(manualDepends))
